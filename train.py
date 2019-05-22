afc6bf2bddb6fe60ed6b397c53b5c532b9d2727a
unknown <gaoqian@buaa.edu.cn>
2019-05-22 15:16:43 +0800

    add train.py with bash

---
+++ train.py
@@ -0,0 +1,27 @@
+
+
+
+import data
+import model
+import project
+
+
+
+def train(dataset, point_net, image_net, optimizer):
+	projector = project.Projector()
+	for _ in range(epoches):
+		for batch in dataset:
+			point_pred = point_net(batch['x'])
+			images = projector.project(point_pred)
+			pred = image_net(images)
+			loss = loss(pred, batch['y'])
+			optimizer.optimize(loss)
+
+
+if __name__ == '__main__':
+
+	dataset = data.Dataset()
+	point_net = model.PointNet()
+	image_net = model.ImageNet()
+	optimizer = tf.AdamOptimizer()
+	train(dataset, point_net, image_net, optimizer)
